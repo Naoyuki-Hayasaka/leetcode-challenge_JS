fb7db3187feec292310dc47acaee1a599fdd36cc
Naoyuki Hayasaka <lelivredes5anneaux@gmail.com>
2022-04-06 01:10:43 +0900

    modified No.852's function

--- src/852. Peak Index in a Mountain Array/index.js
+++ src/852. Peak Index in a Mountain Array/index.js
@@ -1,15 +1,16 @@
 const peakIndexInMountainArray = (arr) => {
   let left = 0;
   let right = arr.length - 1;
-  while (left < right) {
+  while (left <= right) {
     let mid = Math.trunc((left + right) / 2);
-    if (arr[mid] < arr[mid + 1]) {
+    if (arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1]) {
+      return mid;
+    } else if (arr[mid - 1] < arr[mid]) {
       left = mid + 1;
     } else {
-      right = mid;
+      right = mid - 1;
     }
   }
-  return left;
 };
 
 module.exports = {
--- src/852. Peak Index in a Mountain Array/spec.js
+++ src/852. Peak Index in a Mountain Array/spec.js
@@ -18,4 +18,16 @@ describe("852. Peak Index in a Mountain Array", () => {
     const arr = [0, 1, 2, 3, 4, 5, 4, 3];
     expect(peakIndexInMountainArray(arr)).to.equal(5);
   });
+  it("should be return 1", () => {
+    const arr = [5, 10, 9, 5, 4, 3, 2, 0];
+    expect(peakIndexInMountainArray(arr)).to.equal(1);
+  });
+  it("should be return 1", () => {
+    const arr = [9, 10, 9, 8, 7, 6, 5, 4, 3];
+    expect(peakIndexInMountainArray(arr)).to.equal(1);
+  });
+  it("should be return 6", () => {
+    const arr = [3, 5, 7, 9, 10, 11, 13, 11, 10];
+    expect(peakIndexInMountainArray(arr)).to.equal(6);
+  });
 });
